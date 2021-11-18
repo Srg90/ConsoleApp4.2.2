a6b332418a25e8d1e60e817ed30fd8fb01fa6989
Srg90 <usikov90@mail.ru>
2021-11-19 00:56:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите любое количество чисел");
+            Console.WriteLine();
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            int kp = 0, a;
+            int ko = 0;
+            int n1 = 0;
+            Console.WriteLine();
+
+
+            do
+            {
+                Console.Write("Введите число - ");
+                a = Convert.ToInt32(Console.ReadLine());
+                n1++;
+                if (a > 0)
+                    kp += i;
+                if (a < 0)
+                    ko += i;
+
+            } while (n1 < n);
+
+            Console.WriteLine();
+            Console.WriteLine("Количество положительных чисел = " + kp);
+            Console.WriteLine();
+            Console.WriteLine("Количество отрицательных чисел = " + ko);
+            Console.WriteLine();
+            if (kp > ko)
+            {
+                Console.WriteLine("Количество положительных чисел больше чем отрицательных");
+            }
+            if (kp < ko)
+            {
+                Console.WriteLine("Количество отрицательных чисел больше чем положительных");
+            }
+            if (kp == ko)
+            {
+                Console.WriteLine("Количество положительных чисел равно количеству отрицательных");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
+        
+    
+
